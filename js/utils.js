1eb869967f6d86517d384b9eebe475ed7c322f27
sintaxe-proj <josimarkapps10@gmail.com>
2026-05-24 10:54:05 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,11 +3,7 @@
    ========================================================================== */
 
 function mostrarToast(mensagem) {
-    const toast =
-        document.getElementById(
-            "toastNotification"
-        );
-
+    const toast = document.getElementById("toastNotification");
     if (!toast) return;
 
     toast.innerText = mensagem;
@@ -19,39 +15,44 @@ function mostrarToast(mensagem) {
 }
 
 function mascaraCPF(campo) {
-    let v =
-        campo.value.replace(/\D/g, "");
+    let v = campo.value.replace(/\D/g, "");
 
-    if (v.length > 11) {
-        v = v.substring(0,11);
+    if (v.length > 11) v = v.substring(0, 11);
+
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+
+    campo.value = v;
+}
+
+function mascaraCNS(campo) {
+    let v = campo.value.replace(/\D/g, "");
+
+    if (v.length > 15) v = v.substring(0, 15);
+
+    campo.value = v;
+}
+
+function mascaraTelefone(campo) {
+    let v = campo.value.replace(/\D/g, "");
+
+    if (v.length > 11) v = v.substring(0, 11);
+
+    if (v.length <= 10) {
+        v = v.replace(/(\d{2})(\d)/, "($1) $2");
+        v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    } else {
+        v = v.replace(/(\d{2})(\d)/, "($1) $2");
+        v = v.replace(/(\d{5})(\d)/, "$1-$2");
     }
-
-    v = v.replace(
-        /(\d{3})(\d)/,
-        "$1.$2"
-    );
-
-    v = v.replace(
-        /(\d{3})(\d)/,
-        "$1.$2"
-    );
-
-    v = v.replace(
-        /(\d{3})(\d{1,2})$/,
-        "$1-$2"
-    );
 
     campo.value = v;
 }
 
 function mostrarCard(id, valor) {
-    const elemento =
-        document.getElementById(id);
-
+    const elemento = document.getElementById(id);
     if (!elemento) return;
 
-    elemento.style.display =
-        valor === "Sim"
-            ? "block"
-            : "none";
+    elemento.style.display = valor === "Sim" ? "block" : "none";
 }
