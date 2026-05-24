3a2953a5efbb621c110bec51fe4aef883d0c1a2c
sintaxe-proj <josimarkapps10@gmail.com>
2026-05-24 06:54:27 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "COLE_AQUI_PROJECT_URL";
+const SUPABASE_ANON_KEY = "COLE_AQUI_ANON_PUBLIC_KEY";
 
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
